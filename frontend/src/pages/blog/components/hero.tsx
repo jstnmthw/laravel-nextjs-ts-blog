24a7b50d600a0f32cc122928d12e4eb28d35352a
Justin Mathew <web@jstn.ly>
2022-04-09 23:39:35 +0700

    update: blog section

---
+++ frontend/src/pages/blog/components/hero.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const Hero: FC = () => {
+    return (
+        <div className="my-20 text-center">
+            <h1 className="mb-3 text-4xl font-black">Blog</h1>
+            <span className="text-xl dark:text-gray-400">
+                Scribbles from a full stack developer.
+            </span>
+        </div>
+    )
+}
+export default Hero
--- frontend/src/pages/blog/index.tsx
+++ frontend/src/pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { useAuth } from '@/hooks/auth'
 import Navigation from '@/components/Layouts/Navigation'
+import Hero from '@/pages/blog/components/hero'
 
 export default function Home() {
     const { user } = useAuth()
@@ -10,6 +11,9 @@ export default function Home() {
                 <title>Blog | justin.ly</title>
             </Head>
             <Navigation user={user} />
+            <main>
+                <Hero />
+            </main>
         </div>
     )
 }
